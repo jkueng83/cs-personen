c601543ca9e51cb9dad5dd6b25216b738759aa7c
jkueng83 <johannes.kueng@gmx.at>
2020-10-17 13:34:35 +0200

    first commit

---
+++ Personen/Klassen/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Personen.Klassen
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string job;
+
+        public Person(string name, int age, string city, string job)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.job = job;
+        }
+
+        public string Name
+        {
+
+            get { return this.name; }
+            set { this.name = value; }
+
+
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public string City
+        {
+            get { return this.city; }
+            set { this.city = value; }
+        }
+
+        public string Job
+        {
+            get { return this.job; }
+            set { this.job = value; }
+        }
+
+
+
+    }
+}
---
+++ Personen/Program.cs
@@ -0,0 +1,62 @@
+﻿using Personen.Klassen;
+using System;
+using System.Collections.Generic;
+
+namespace Personen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! - Aufgabe: Personen");
+
+            string[] fileContent = System.IO.File.ReadAllLines("C:\\03_Coding\\02 Projects\\CS\\Personen\\persons.txt");
+
+            List<Person> people = new List<Person>();
+
+            foreach (string s in fileContent)
+            {
+                Console.WriteLine(s);
+
+                string[] stringArray = s.Split(";");
+
+                string name = stringArray[0];
+                int age = int.Parse(stringArray[1]);
+                string city = stringArray[2];
+                string job = "";
+
+
+                switch (name)
+                {
+                    case "Helga":
+                    case "Hans":
+                        job = "Coder";
+                        break;
+                    default:
+                        job = "Officer";
+                        break;
+                }
+
+                Person person1 = new Person(name, age, city, job);
+
+                people.Add(person1);
+
+            }
+
+
+            int counter = 0;
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.Name + p.Age + p.City + p.Job);
+
+                fileContent[counter] = p.Name + ";" + p.Age + ";" + p.City + ";" + p.Job;
+
+                counter = counter + 1;
+            }
+
+            System.IO.File.WriteAllLines("C:\\03_Coding\\02 Projects\\CS\\Personen\\persons1.txt", fileContent);
+
+        }
+    }
+}
